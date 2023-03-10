3b5a0e0f84157de416cde9d14a8dcc640816109a
Clément Fiere <clement.fiere@eficode.com>
2023-03-10 09:46:31 +0200

    initial

---
+++ 101_watcher.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import os
+from time import sleep
+import datetime
+
+hostname = "192.168.0.1"
+# hostname = "home.fiere.fr"
+vm_id = 100
+interval = 5
+deadline = 30
+add_ts = False
+
+
+def f_print(*args, **kwargs):
+    print(*args, **kwargs, flush=True)
+
+
+def ts_print(*args, **kwargs):
+    if add_ts:
+        f_print(str(datetime.datetime.now())[:-4], *args, **kwargs)
+    else:
+        f_print(*args, **kwargs)
+
+
+def get_status():
+    return os.popen('qm status %s' % vm_id).read()
+
+
+def is_running():
+    return str(get_status()).replace('\n', '').strip() == 'status: running'
+
+
+def is_host_up(deadline=3):
+    return os.system("ping -w %d -c 1 %s >/dev/null" % (deadline, hostname)) == 0
+
+
+def wait_for_host_to_be_online():
+    if not is_host_up(1):
+        ts_print('waiting for host %s to come up' % hostname)
+        while not is_host_up(1):
+            f_print('.', end='')
+        f_print('')
+
+
+def wait_for_vm_to_be_up():
+    if not is_running():
+        ts_print("VM is down, waiting for it to start")
+        while not is_running():
+            f_print('.', end='')
+            sleep(1)
+        f_print('')
+        ts_print('VM is running !')
+
+
+def cycle_vm():
+    ts_print('Restarting VM %d' % vm_id)
+    ret = os.system('qm stop %d && qm start %d' % (vm_id, vm_id))
+    ts_print('(%d) done' % ret)
+
+
+def ping():
+    if is_host_up(deadline):
+        ts_print(hostname, 'is up :D')
+    else:
+        ts_print(hostname, 'is down !')
+        cycle_vm()
+        wait_for_host_to_be_online()
+        ping()
+
+
+def watchdog():
+    wait_for_vm_to_be_up()
+    wait_for_host_to_be_online()
+    while True:
+        ping()
+        sleep(interval)
+
+
+if __name__ == '__main__':
+    watchdog()
